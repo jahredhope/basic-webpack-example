86e765f3066988d588dbf4a0cdb5d8bad402a6bc
Jahred Hope <jahredhope@gmail.com>
2019-08-11 12:08:24 +1000

    Add Puppeteer config

--- jest.config.js
+++ jest.config.js
@@ -1,11 +1,13 @@
 module.exports = {
-  preset: 'ts-jest/presets/js-with-babel',
-  testEnvironment: 'jsdom',
+  preset: "ts-jest/presets/js-with-babel",
   globals: {
     "ts-jest": {
       diagnostics: {
         warnOnly: true
       }
     }
-  }
+  },
+  globalSetup: "./jest/setup.js",
+  globalTeardown: "./jest/teardown.js",
+  testEnvironment: "./jest/puppeteer_environment.js"
 };
